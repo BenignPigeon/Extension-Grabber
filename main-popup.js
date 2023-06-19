3eba187ed7df53fcde39eeffb2cf3e0256e70c91
Ben Cohen <bencohen1105@gmail.com>
2023-06-19 20:58:29 +0100

    Update main-popup.js

--- main-popup.js
+++ main-popup.js
@@ -1,4 +1,4 @@
-document.getElementById('generateButton').addEventListener('click', function() {
+document.getElementById('generateHTMLButton').addEventListener('click', function() {
   chrome.runtime.sendMessage({ action: "generateLinks" }, function(response) {
     chrome.action.setBadgeText({ text: "" });
   });
